288d692f4445102e4157ec2b4586f01cc374748b
Arigaran M <arigaranpdy@gmail.com>
2024-10-29 10:50:13 +0530

    Day 1 of TNS JAVA Project.

---
+++ TNS_JAVA_Project/src/day1/AdditionDemo.java
@@ -0,0 +1,17 @@
+package day1;
+import java.util.*;
+public class AdditionDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		Scanner sr = new Scanner(System.in);
+		System.out.print("Enter the a value: ");
+		a=sr.nextInt();
+		System.out.print("Enter the b value: ");
+		b=sr.nextInt();
+		c=a+b;
+		System.out.println("The Output Is: "+c);
+	}
+	
+}
